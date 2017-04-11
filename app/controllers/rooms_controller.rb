563139f4b723f3f9961cfa16146f93051c074d2f
osboxes.org <osboxes.henrygarcia0309@gmail.com>
2017-04-11 18:06:44 -0500

    Challenge rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -16,6 +16,29 @@ class RoomsController < ApplicationController
     end
   end
 
+  def edit
+    @room = Room.find(params[:id])
+    @title = @room.title
+    @description = @room.description
+    @beds =  @room.beds
+    @guests =  @room.guests
+    @image_url =  @room.image_url
+  end
+
+def update
+  @title = params[:room][:title]
+  @description = params[:room][:description]
+  @beds = params[:room][:beds]
+  @guests = params[:room][:guests]
+  @image_url = params[:room][:image_url]
+  if @room.save
+    redirect_to rooms_path
+  else
+    render "edit"
+  end
+
+end
+
   protected
     def room_params
       params.require(:room).permit(:title, :description, :beds, :guests, :image_url)
